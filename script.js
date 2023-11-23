06365a675a6187486975ca6da9c45bab530f60cd
Marc <marc@greatapegames.com>
2023-11-23 11:20:36 +0000

    fix

--- script.js
+++ script.js
@@ -1,56 +1,54 @@
-let currentPage = 1;
-const pageTurnSound = new Audio('page-turn.wav'); // Path to page turning sound
-let pagesText = [];
-let currentNarration = null; // Reference to the current narration audio
+document.addEventListener('DOMContentLoaded', (event) => {
+    let currentPage = 0;
+    const pageTurnSound = new Audio('page-turn.wav'); // Path to page turning sound
+    let pagesText = [];
+    let currentNarration = null; // Reference to the current narration audio
 
-function updatePages() {
-    document.getElementById('leftPageImage').style.backgroundImage = `url('images/page${currentPage}.jpg')`;
-    document.getElementById('rightPageImage').style.backgroundImage = `url('images/page${currentPage + 1}.jpg')`;
-    updateText();
-}
+    function updatePages() {
+        document.getElementById('pageImage').style.backgroundImage = `url('images/${currentPage}.png')`;
 
-function playNarration(pageNumber) {
-    if (currentNarration) {
-        currentNarration.pause(); // Stop any currently playing narration
+        var text = currentPage < pagesText.length ? pagesText[currentPage] : ''
+        document.getElementById('pageText').innerText = text;
     }
-    currentNarration = new Audio(`audio/${pageNumber}.mp3`);
-    currentNarration.play();
-}
 
-document.getElementById('leftPage').addEventListener('click', () => {
-    if (currentPage > 1) {
-        currentPage -= 2;
-        updatePages();
-        // Not replaying audio
-    }
-});
-
-document.getElementById('rightPage').addEventListener('click', () => {
-    if (currentPage < pagesText.length) {
-        currentPage += 2;
-        updatePages();
-        pageTurnSound.play();
-        playNarration(currentPage);
-    }
-});
-
-function updateText() {
-    const leftPageIndex = currentPage - 1;
-    const rightPageIndex = currentPage;
-
-    // Update text for the left page
-    document.getElementById('leftPageText').innerText = leftPageIndex < pagesText.length ? pagesText[leftPageIndex] : '';
-
-    // Update text for the right page
-    document.getElementById('rightPageText').innerText = rightPageIndex < pagesText.length ? pagesText[rightPageIndex] : '';
-}
-
-// Load text content and initialize pages
-fetch('text.txt').then(response => response.text()).then(text => {
-    pagesText = text.split('\n\n');
+    function playNarration(pageNumber) {
+        if (currentNarration) {
+            currentNarration.pause(); // Stop any currently playing narration
+            currentNarration.currentTime = 0; // Reset the audio playback
+        }
+        currentNarration = new Audio(`audio/${pageNumber}.mp3`);
     
-    // Initialize pages and text for the first time
-    updatePages(); // Moved this call after setting pagesText
-}).catch(error => {
-    console.error('Error loading text:', error);
+        // Delay playing the narration
+        setTimeout(() => {
+            currentNarration.play();
+        }, 1500); // Delay of 1000 milliseconds (1 second)
+    }
+
+    document.getElementById('pageText').addEventListener('click', () => {
+        if (currentPage > 0) {
+            currentPage -= 1;
+            updatePages();
+            pageTurnSound.play();
+            playNarration(currentPage);
+        }
+    });
+
+    document.getElementById('pageImage').addEventListener('click', () => {
+        if (currentPage + 1 < pagesText.length) {
+            currentPage += 1;
+            updatePages();
+            pageTurnSound.play();
+            playNarration(currentPage);
+        }
+    });
+
+    // Load text content and initialize pages
+    fetch('text.txt').then(response => response.text()).then(text => {
+        pagesText = text.split('@');
+        console.log(pagesText)
+        // Initialize pages and text for the first time
+        updatePages(); // Moved this call after setting pagesText
+    }).catch(error => {
+        console.error('Error loading text:', error);
+    });
 });
